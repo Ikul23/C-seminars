b2c5771f7f0018878708ab19e85d021ad5293138
Ikul23 <ikul23@icloud.com>
2023-11-19 14:21:51 +0300

    Added modified project H001M

--- HT001/Program.cs
+++ HT001/Program.cs
@@ -1,15 +1,35 @@
 ﻿// Задача 1: Написать программу, которая на вход принимает два числа 
 // и выдает какое число большее, а какое меньшее
+using System;
 
-int a = 0;
-int b = 0;
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+        }
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+        }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+        }
+    }
 
-if (a == b){
-    Console.WriteLine($" Числа равны между собой!"); // проверка на равенство чисел 
-}
-if (a > b){
-    Console.WriteLine($"max={a} min={b}");
-}
-else if(b > a){
-    Console.WriteLine($"max={b} min={a}");
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+            firstNumber = 15;
+            secondNumber = 5;
+        }
+
+        CompareNumbers(firstNumber, secondNumber);
+    }
 }
