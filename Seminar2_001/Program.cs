56894e959875343828c84165d09e75c79f1fe4b2
Ikul23 <ikul23@icloud.com>
2023-11-22 21:03:41 +0300

    Added new home tasks projects HT005-HT008

---
+++ Seminar2_001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача: на вход принимае трехзначное 
+//число и удаляет вторую цифру из этого числа
+int num = 456;
+int first = num / 100;
+int second = num % 100; // подставляем остаток деления на 10
+//Console.WriteLine($"{first}{second}");
+int result = first*10 + second;
+Console.WriteLine($"{result}");
---
+++ Seminar2_002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре.
+// Примеры
+//487 => 8^7 = 2 097 152
+//254 => 5^4 = 625
+//617 => 1
+// int num = 254;
+
+// int second = (num / 10) % 10;
+// int power = num % 10;
+
+// int count = 0;
+// int result = 1;
+
+// while (count < power) {
+//     result = second * result;
+//     count++;
+// }
+
+// Console.WriteLine(result);
+
+int num = 254;
+
+int second = (num / 10) % 10;
+int power = num % 10;
+
+double result = Math.Pow(second, power);
+
+Console.WriteLine(result);
+
